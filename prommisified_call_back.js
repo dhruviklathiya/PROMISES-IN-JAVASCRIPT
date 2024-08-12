17785f5911315358ce813aeb9987059dd92734fd
Dr. D CODER <dhruviklathiya9999@gmail.com>
2024-08-12 13:51:48 +0530

    Simple code

--- prommisified_call_back.js
+++ prommisified_call_back.js
@@ -1,32 +1,41 @@
-// Assignment
+// Method1:
+const starting_function = (resolve) => {
+    console.log("===> Starting function");
+    setTimeout(() => {
+        console.log("hi");
+        resolve();  // Resolve the promise to continue the chain
+    }, 1000);
+};
 
-const starting_function = (next_argument_from_chain) => {
-    console.log("===> Starting function")
-    setTimeout(() => {
-        console.log("hi")
-        next_argument_from_chain()
-    }, 1000)
-}
+const function_3s = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("hello");
+            resolve();  // Resolve the promise to continue the chain
+        }, 3000);
+    });
+};
 
-const function_3s = (next_argument_from_chain) => {
-    setTimeout(() => {
-        console.log("hello")
-        next_argument_from_chain
-    }, 3000)
-}
-const function_5s = (next_argument_from_chain2) => {
-    setTimeout(() => {
-        console.log("hi there")
-        next_argument_from_chain2
-    }, 5000)
-}
+const function_5s = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("hi there");
+            resolve();  // Resolve the promise to continue the chain
+        }, 5000);
+    });
+};
 
 const another_function = () => {
-    setTimeout(() => {
-        console.log("END")
-    }, 7000)
-}
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("END");
+            resolve();  // Resolve the promise to finish the chain
+        }, 7000);
+    });
+};
 
-
-const d = new Promise(starting_function)
-d.then(function_3s).then(function_5s).then(another_function) // Considering this as chain
+// Start the promise chain
+new Promise(starting_function)
+    .then(function_3s)
+    .then(function_5s)
+    .then(another_function);
