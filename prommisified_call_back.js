80aa59394da2dab70fd85b6181a2a2a508d997a8
Dr. D CODER <dhruviklathiya9999@gmail.com>
2024-08-12 11:51:42 +0530

    Promises bug resolve

--- prommisified_call_back.js
+++ prommisified_call_back.js
@@ -5,6 +5,7 @@ const starting_function = (next_argument_from_chain) => {
     setTimeout(() => {
         console.log("hi")
         next_argument_from_chain()
+        // next_argument_from_chain
     }, 1000)
 }
 
@@ -14,10 +15,19 @@ const function_3s = (next_argument_from_chain) => {
         next_argument_from_chain
     }, 3000)
 }
-const function_5s = () => {
-    return (setTimeout(() => { console.log("hi there") }, 5000))
+const function_5s = (next_argument_from_chain2) => {
+    setTimeout(() => {
+        console.log("hi there")
+        next_argument_from_chain2
+    }, 5000)
+}
+
+const another_function = () => {
+    setTimeout(() => {
+        console.log("END")
+    }, 7000)
 }
 
 
 const d = new Promise(starting_function)
-d.then(function_3s).then(function_5s) // Considering this as chain
+d.then(function_3s).then(function_5s).then(another_function) // Considering this as chain
